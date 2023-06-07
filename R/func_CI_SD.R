cd9294bef9eeec981310eeabf660fcceae2dc923
Shinichi Nakagawa <s.nakagawa@unsw.edu.au>
2023-06-07 09:30:48 +0900

    Update func_CI_SD.R

--- R/func_CI_SD.R
+++ R/func_CI_SD.R
@@ -1,8 +1,15 @@
 # conversion CI to SD
+# I need a function to convert CI to SD using t distribution
 
 ci_to_sd <-function(upper, lower, n){
-  
+    # upper: upper limit of CI
+    # lower: lower limit of CI
+    # n: sample size
+    # return: SD
+    # SD = (upper - lower) / (2 * qt(0.975, n-1))
+    return((upper - lower) / (2 * qt(0.975, n-1)))
 }
 
-# testing
+# testing code for ci_to_sd
+#ci_to_sd(upper = 0.5, lower = 0.4, n = 10)
 
