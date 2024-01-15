79a8065fc35d0157060c2b48a2737283da29606c
April Martinig <50909009+martinig@users.noreply.github.com>
2024-01-15 15:27:07 +1100

    updated data

--- R/functions/func_median&IQR_to_mean&SD.R
+++ R/functions/func_median&IQR_to_mean&SD.R
@@ -1,14 +1,16 @@
 # function for getting mean and sd from median, IQR, and sample size
 
 mean_sd_from_iqr <- function(n, med, iqr) {
-  mean_val <- med
-  sd_val <- iqr / (2 * qnorm(0.75))
+	a <- med - (iqr/2) 
+	b <- med + (iqr/2)
+  mean_val <- (a + med + b)/3
+  sd_val <- iqr / 1.35
   return(list(mean = mean_val, sd = sd_val))
 }
 
 # estimation:
-n <- 106
-med <- 33
+n <- 145
+med <- 29
 iqr <- 24
 
 result <- mean_sd_from_iqr(n, med, iqr)
@@ -16,9 +18,9 @@ cat("Mean:", result$mean, "\n")
 cat("Standard Deviation:", result$sd, "\n")
 
 # estimation:
-n <- 71
-med <- 34
-iqr <- 25
+n <- 78
+med <- 28
+iqr <- 20.5
 
 result <- mean_sd_from_iqr(n, med, iqr)
 cat("Mean:", result$mean, "\n")
