3df0aee6438ba1b9dc0dedb269ded94a50091d38
April Martinig <50909009+martinig@users.noreply.github.com>
2025-11-27 12:55:24 -0800

    added Reynolds et al. 2025

--- R/extracting_data_from_figures.R
+++ R/extracting_data_from_figures.R
@@ -1,14 +1,18 @@
 #devtools::install_github("daniel1noble/metaDigitise")
 library(metaDigitise)
-library(dplyr), for
+library(dplyr)
 
-data <- metaDigitise(dir = "~/Documents/Files/Post-docs/UNSW 2022-2024/Aim 1/Extracting data from figures/")
+data <- metaDigitise(dir = "~/Documents/Files/Manuscripts/Martinig et al. 2025 (meta analysis)/Extracting data from figures/")
 
 #helpful webpage: https://cran.r-project.org/web/packages/metaDigitise/vignettes/metaDigitise.html
 
 data 
 
-data%>%filter(filename=="Martin et al. 2014.png")
+data%>%filter(filename=="Reynolds et al. 2025femalenondis.png")
+data%>%filter(filename=="Reynolds et al. 2025femaledis.png")
+data%>%filter(filename=="Reynolds et al. 2025malenondis.png")
+data%>%filter(filename=="Reynolds et al. 2025maledis.png")
+
 data%>%filter(filename=="Veenstra & Byrne 1999 SE.png")
 data%>%filter(filename=="Ward & Weatherhead 2005.png")
 data%>%filter(filename=="Williams & Rabenold 2005 SD?.png")
